882de500c37194ae0cafa6915708f31e21364d24
mgkim-developer <mgkim.developer@gmail.com>
2021-08-25 21:43:15 +0900

    2021.08.25_(5-4 Handling exceptions) Review continues3.

--- (5-4)Handling exceptions.py
+++ (5-4)Handling exceptions.py
@@ -102,3 +102,18 @@ except ZeroDivisionError as e:
 
 
 # try .. finally
+
+# try문에는 finally절을 사용할 수 있다. finally절은 try문 수행 도중 예외 발생 여부에 상관없이 항상 수행된다. 보통 finally절은 사용한 리소스를 close해야 할 때에 많이 사용한다.
+
+# 다음 예를 보자.
+
+# f = open('foo.txt', 'w')
+# try:
+#     # 무언가를 수행한다.
+# finally:
+#     f.close()
+
+# foo.txt 파일을 쓰기 모드로 연 후에 try문을 수행한 후 예외 발생 여부와 상관없이 finally절에서 f.close()로 열린 파일을 닫을 수 있다.
+
+
+# 여러개의 오류처리하기
